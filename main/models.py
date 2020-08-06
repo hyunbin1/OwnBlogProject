58432df0e300744004d224431a0f5788a8562b93
hyunbin1 <kimgusqls1@gmail.com>
2020-08-06 18:05:47 +0900

    solve problem to create object with blank=True

--- main/models.py
+++ main/models.py
@@ -5,6 +5,6 @@ from django.db import models
 # 게시물
 class Ob(models.Model):
     title = models.CharField(max_length=50)
-    image = models.ImageField(null=True)
-    content = models.TextField(null=True)
+    image = models.ImageField(null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
     update_at = models.DateTimeField(auto_now=True)
