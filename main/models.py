b7c1aa2ef7262a8ad3dc7e24edf90426d9a94223
hyunbin1 <kimgusqls1@gmail.com>
2020-08-09 14:57:06 +0900

    fix error

--- main/models.py
+++ main/models.py
@@ -7,4 +7,4 @@ class Ob(models.Model):
     title = models.CharField(max_length=50)
     image = models.ImageField(null=True, blank=True)
     content = models.TextField(null=True, blank=True)
-    update_at = models.DateTimeField(auto_now=True)
+    update_at = models.DateTimeField(auto_now=True) 
