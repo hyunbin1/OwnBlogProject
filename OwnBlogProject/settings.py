e154d897c35feb3e922a55661db113208f40894d
hyunbin1 <kimgusqls1@gmail.com>
2020-08-11 15:54:17 +0900

    added media url

--- OwnBlogProject/settings.py
+++ OwnBlogProject/settings.py
@@ -125,8 +125,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,15 +1,8 @@
 from django.urls import path
-# from django.contrib.auth.views import LoginView, LogoutView - 로그인 커스텀 안할때 
-# 로그인 커스텀 할때 LoginView 삭제해줌
-from .views import signup, MyLoginView
-from django.contrib.auth.views import LogoutView
+from django.urls import path
+from .views import login, signup, logout
 
 urlpatterns = [
-    path('signup/', signup, name="signup"),
-
-   # path('login/' ,LoginView.as_view(), name="login"), - 기존 장고에 있는 폼 가져오는 법
-   # 커스텀 해준 로그인 뷰 가져오는법
-    path('login/',MyLoginView.as_view(), name="login"),
-    path('logout/',LogoutView.as_view(), name="logout"),
-
-]
+    path('login/', login, name="login"),
+    path('signup/', login, name="signup"),
+    path('logout/', login, name="logout"),
