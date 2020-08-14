56a8a1e0aa54f5d2700cacf57f01050d82e1b805
seunghyun2269 <akdntm2269@naver.com>
2020-08-15 00:53:45 +0900

    edit models

--- main/models.py
+++ main/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 
-
-# 게시물
 class Ob(models.Model):
     title = models.CharField(max_length=50)
     image = models.ImageField(null=True)
     content = models.TextField(null=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+        
