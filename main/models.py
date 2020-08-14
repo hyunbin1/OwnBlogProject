9cac8453898f012a94f54a219ac027c81424f33a
seunghyun2269 <akdntm2269@naver.com>
2020-08-15 01:03:59 +0900

    edit models

--- main/models.py
+++ main/models.py
@@ -3,16 +3,9 @@ from django.db import models
 
 class Ob(models.Model):
     title = models.CharField(max_length=50)
-<<<<<<< HEAD
-    image = models.ImageField(null=True)
+    image = models.ImageField(blank=True)
     content = models.TextField(null=True)
     update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
-        
-=======
-    image = models.ImageField(null=True, blank=True)
-    content = models.TextField(null=True, blank=True)
-    update_at = models.DateTimeField(auto_now=True) 
->>>>>>> 4add3c4a8e1ced17788c1f06ae43396bb5e18afc
