f2db4f45ecaabef841deb7e2420565804fa25431
hyunbin1 <kimgusqls1@gmail.com>
2020-08-16 23:04:36 +0900

    fix url'main2'

--- main/urls.py
+++ main/urls.py
@@ -3,10 +3,10 @@ from .views import index, create, detail, delete, update, main2
 
 
 urlpatterns = [
-    path('', index, name="index"),
+    path('', main2, name="main2"),
+    path('index/', index, name="index"),
     path('create/', create, name="create"),
     path('detail/<int:Ob_id>', detail, name="detail"),
     path('delete/<int:Ob_id>', delete, name="delete"),
     path('update/<int:Ob_id>', update, name="update"),
-    path('main2/<int:Ob_id>', main2, name="main2"),
 ]
