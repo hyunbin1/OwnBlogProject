799b3ee331586e85ae53569fea8b796901c114f5
seunghyun2269 <akdntm2269@naver.com>
2020-08-18 17:22:30 +0900

    edit urls.py

--- main/urls.py
+++ main/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('', main2, name="main2"),
     path('index/', index, name="index"),
     path('create/', create, name="create"),
-    path('new/', new, name = 'new'),
     path('detail/<int:Ob_id>', detail, name="detail"),
     path('delete/<int:Ob_id>', delete, name="delete"),
     path('update/<int:Ob_id>', update, name="update"),
